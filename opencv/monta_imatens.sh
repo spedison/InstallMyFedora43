32756442b51046534849cb40d27b094e315d0d14
EDISON RIBEIRO ARAUJO <spedison@users.noreply.github.com>
2025-12-27 12:08:27 -0300

    Refactor mont_imatens.sh for clarity and updates

--- opencv/monta_imatens.sh
+++ opencv/monta_imatens.sh
@@ -1,18 +1,19 @@
-podman build -f ./Dockerfile.ubuntu_opencv -t  ubuntu_opencv:1.0 .
-podman build -f ./Dockerfile.fedora_opencv -t  fedora_opencv:1.0 .
+# criando as imagens
+podman build -f Dockerfile.ubuntu_opencv -t ubuntu_opencv:1.0 .
 
-# Criando um toolbox com o Ubuntu
-distrobox create --yes --nvidia \
---name ubuntu_opencv \
---image localhost/ubuntu_opencv:1.0  \
---name ubuntu_opencv
+podman build -f Dockerfile.fedora_opencv -t fedora_opencv:1.0 .
 
-distrobox create --yes --nvidia \
---name fedora_opencv \
---image localhost/fedora_opencv:1.0  \
---name fedora_opencv
+# Criando os containeres
+distrobox create --yes --nvidia --name ubuntu_opencv --image localhost/ubuntu_opencv:1.0
 
-# Roda o setup para deixá-los pronto para uso.
-distrobox upograde ubuntu_opencv 
+distrobox create --yes --nvidia --name fedora_opencv --image localhost/fedora_opencv:1.0
 
-distrobox upograde fedora_opencv 
+# Atualiza os containeres e o Setup
+distrobox upgrade ubuntu_opencv
+
+distrobox upgrade fedora_opencv
+
+# Para usar os containeres
+# distrobox enter ubuntu_opencv
+# distrobox enter fedora_opencv
+
