7af885722082c3cff57f5b48d86362589db7a9ea
EDISON RIBEIRO ARAUJO <spedison@users.noreply.github.com>
2025-12-29 21:31:10 -0300

    Refactor libvirt installation script
    Refactor installation script to check for existing packages before installation and improve package management.

--- install_libvirt.sh
+++ install_libvirt.sh
@@ -1,29 +1,59 @@
-sudo rpm-ostree -y install \
-  libvirt \
-  virtiofsd \
-  virt-manager \
-  virt-viewer \
-  virt-top \
-  virt-install \
-  qemu-kvm \
-  libvirt-daemon-config-network \
-  libvirt-daemon-kvm \
-  libvirt-daemon-driver-qemu \
-  libvirt-daemon-driver-network \
-  libguestfs-tools \
-  virt-sysprep \
-  virt-clone \
-  virt-rescue \
-  virt-builder \
-  bridge-utils \
-  dnsmasq \
-  virt-what \
-  virt-sandbox \
-  libvirt-client \
-  virt-v2v \
-  virt-v2v-bash-completion \
+#!/bin/bash
+
+declare -a all_packages=()
+
+# Função para verificar e instalar pacotes rpm-ostree somente se não estiverem instalados
+install_if_not_exists() {
+    local packages=("$@")
+    local to_install=()
+
+    for pkg in "${packages[@]}"; do
+        if rpm -q "$pkg" &>/dev/null; then
+            echo "[✔] Pacote '$pkg' já está instalado. Ignorando..."
+        else
+            echo "[➕] Pacote '$pkg' será instalado."
+            all_packages+=("$pkg")
+        fi
+    done
+}
+
+install_all_processing() {
+    if [ "${#all_packages[@]}" -gt 0 ]; then
+        echo "📦 Instalando pacotes: ${all_packages[*]}"
+        sudo rpm-ostree install -y "${all_packages[@]}"
+    else
+        echo "✅ Nenhum novo pacote a instalar."
+    fi
+}
+
+declare -a pkgs=(
+  libvirt 
   virtiofsd 
-  
+  virt-manager 
+  virt-viewer 
+  virt-top 
+  virt-install 
+  qemu-kvm 
+  libvirt-daemon-config-network 
+  libvirt-daemon-kvm 
+  libvirt-daemon-driver-qemu 
+  libvirt-daemon-driver-network 
+  libguestfs-tools 
+  virt-sysprep 
+  virt-clone 
+  virt-rescue 
+  virt-builder 
+  bridge-utils 
+  dnsmasq 
+  virt-what 
+  virt-sandbox 
+  libvirt-client 
+  virt-v2v 
+  virt-v2v-bash-completion 
+  virtiofsd )
+
+install_if_not_exists "${pkgs[@]}"
+install_all_processing ""
 
 #sudo usermod -aG libvirt $USER
 
@@ -38,9 +68,6 @@ sudo systemctl enable --now libvirtd
       # Extrai a linha do grupo docker de /usr/lib/group
       if sudo bash -c 'grep "^libvirt:" /usr/lib/group >> /etc/group 2>/dev/null'; then
           echo "  ✅ Grupo 'libvirt' copiado com sucesso mantendo o GID original"
-      #else
-      #    echo "  ⚠️  Não encontrou grupo docker em /usr/lib/group, criando novo..."
-      #    sudo groupadd docker || echo "  ⚠️  Erro ao criar grupo"
       fi
   else
       echo "  ℹ️  Grupo 'libvirt' já existe em /etc/group"
